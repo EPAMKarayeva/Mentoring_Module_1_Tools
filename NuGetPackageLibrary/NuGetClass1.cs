dfd669c55a0ede1f8cdceb952212e35fb97ec0c2
Aziza Karayeva <Aziza_Karayeva@epam.com>
2020-07-10 12:30:11 +0600

    Добавьте файлы проекта.

---
+++ NuGetPackageLibrary/NuGetClass1.cs
@@ -0,0 +1,20 @@
+﻿using NLog;
+using System;
+
+namespace NuGetPackageLibrary
+{
+    public class NuGetClass1
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        private static void Main(string[] args)
+        {
+            logger.Trace("trace message");
+            logger.Debug("debug message");
+            logger.Info("info message");
+            logger.Warn("warn message");
+            logger.Error("error message");
+            logger.Fatal("fatal message");
+        }
+    }
+}
---
+++ NuGetPackageLibrary/NuGetClass2.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NuGetPackageLibrary
+{
+    class NuGetClass2
+    {
+    }
+}
